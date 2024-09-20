af8abcc431d99a700c0e0289ea75d7f19228ca08
azizbibitov11 <azizbibitov11@gmail.com>
2024-09-21 01:43:25 +0500

    initial commit project created

---
+++ PokerSwift/PokerSwift/Core/PokerSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerSwiftApp.swift
+//  PokerSwift
+//
+//  Created by Aziz Bibitov on 21.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PokerSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
